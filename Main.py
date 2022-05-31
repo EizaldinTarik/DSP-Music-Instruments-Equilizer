8a71654a983d7f106c0f05016df5e81109baedf9
EizaldinTarik <Eizaldint@gmail.com>
2022-05-31 16:12:25 +0200

    Final

--- Main.py
+++ Main.py
@@ -1,10 +1,6 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import*
-import winsound
 import matplotlib.ticker as ticker
-from PyQt5 import QtCore as qtc
-from PyQt5 import QtWidgets as qtw
 import pyqtgraph as pg
 from scipy.io import wavfile
 import numpy as np
@@ -16,18 +12,12 @@ matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import sys
-import sourceGuitar
-import pygame
 from Tones import *
-from GUI import Ui_Piano_istrument
-
+#from GUI import Ui_Piano_istrument
 import logging
 logging.basicConfig(level=logging.INFO, filename='logging.log',format='%(asctime)s %(message)s')
                    #datefmt='%m/%D/%Y %I:%M:%S %p', encoding='utf-8')
 
-
-pygame.init()
-pygame.mixer.init()
 
 
 class MplCanvas(FigureCanvas):
@@ -38,14 +28,13 @@ class MplCanvas(FigureCanvas):
         fig.tight_layout()
         
 class MplCanvas_Spec(FigureCanvas):
-	def __init__(self,parent=None, dpi = 120):
-		fig = Figure(dpi = dpi)
-		self.axes = fig.add_subplot(111)
-		super(MplCanvas_Spec,self).__init__(fig)
-		fig.tight_layout()
+    def __init__(self,parent=None, dpi = 120):
+        fig = Figure(dpi = dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas_Spec,self).__init__(fig)
+        fig.tight_layout()
 
 class Worker(QtCore.QRunnable):
-
     def __init__(self, function, *args, **kwargs):
         super(Worker, self).__init__()
         self.function = function
@@ -54,7 +43,7 @@ class Worker(QtCore.QRunnable):
 
     @pyqtSlot()
     def run(self):
-                self.function(*self.args, **self.kwargs)
+        self.function(*self.args, **self.kwargs)
 
 class ApplicationWindow(QtWidgets.QWidget):
     def __init__(self) :
@@ -70,6 +59,25 @@ class ApplicationWindow(QtWidgets.QWidget):
         self.reference_plot = None
         self.q = queue.Queue(maxsize=20)
         self.stop = False
+
+        self.ui.Piano_Button_1.clicked.connect(lambda:pb1(self.ui))
+        self.ui.Piano_Button_2.clicked.connect(lambda:pb2(self.ui))
+        self.ui.Piano_Button_3.clicked.connect(lambda:pb3(self.ui))
+        self.ui.Piano_Button_4.clicked.connect(lambda:pb4(self.ui))
+        self.ui.Piano_Button_5.clicked.connect(lambda:pb5(self.ui))
+        self.ui.Piano_Button_6.clicked.connect(lambda:pb6(self.ui))
+        self.ui.Piano_Button_7.clicked.connect(lambda:pb7(self.ui))
+        self.ui.Piano_Button_above_1.clicked.connect(lambda:pb8(self.ui))
+        self.ui.Piano_Button_above_2.clicked.connect(lambda:pb9(self.ui))
+        self.ui.Piano_Button_above_3.clicked.connect(lambda:pb10(self.ui))
+        self.ui.Piano_Button_above_4.clicked.connect(lambda:pb11(self.ui))
+        self.ui.Piano_Button_above_5.clicked.connect(lambda:pb12(self.ui))
+        self.ui.DrumbButton.clicked.connect(lambda:pb0(self.ui))
+        self.ui.Guitar_button_1.clicked.connect(lambda:g1(self.ui))
+        self.ui.Guitar_button_2.clicked.connect(lambda:g2(self.ui))
+        self.ui.Guitar_button_3.clicked.connect(lambda:g3(self.ui))
+        self.ui.Guitar_button_4.clicked.connect(lambda:g4(self.ui))
+        self.ui.Guitar_button_5.clicked.connect(lambda:g5(self.ui))
 
         #device_info = sd.query_devices(self.ui.device, 'input')
         device_info = sd.query_devices(self.device)
@@ -92,25 +100,6 @@ class ApplicationWindow(QtWidgets.QWidget):
         self.ui.horizontalSlider_For_Guitar.setSliderPosition(1)
         self.ui.horizontalSlider_For_Drumb.setSliderPosition(1)
         self.ui.horizontalSlider_For_piano.setSliderPosition(1)
-
-        # self.ui.Piano_Button_1.clicked.connect(self.ui.pb1)
-        # self.ui.Piano_Button_2.clicked.connect(self.ui.pb2)
-        # self.ui.Piano_Button_3.clicked.connect(self.ui.pb3)
-        # self.ui.Piano_Button_4.clicked.connect(self.ui.pb4)
-        # self.ui.Piano_Button_5.clicked.connect(self.ui.pb5)
-        # self.ui.Piano_Button_6.clicked.connect(self.ui.pb6)
-        # self.ui.Piano_Button_7.clicked.connect(self.ui.pb7)
-        # self.ui.Piano_Button_above_1.clicked.connect(self.ui.pb8)
-        # self.ui.Piano_Button_above_2.clicked.connect(self.ui.pb9)
-        # self.ui.Piano_Button_above_3.clicked.connect(self.ui.pb10)
-        # self.ui.Piano_Button_above_4.clicked.connect(self.ui.pb11)
-        # self.ui.Piano_Button_above_5.clicked.connect(self.ui.pb12)
-        # self.ui.DrumbButton.clicked.connect(self.ui.pb0)
-        # self.ui.Guitar_button_1.clicked.connect(self.ui.g1)
-        # self.ui.Guitar_button_2.clicked.connect(self.ui.g2)
-        # self.ui.Guitar_button_3.clicked.connect(self.ui.g3)
-        # self.ui.Guitar_button_4.clicked.connect(self.ui.g4)
-        # self.ui.Guitar_button_5.clicked.connect(self.ui.g5)
 
         self.ui.pushButton_pause.clicked.connect(self.stop_media)
         self.ui.pushButton_Play.clicked.connect(self.play_media)
@@ -174,7 +163,6 @@ class ApplicationWindow(QtWidgets.QWidget):
         self.spec_Fig = spec.specgram(self.data, self.fs)
         logging.info('Spectrogram appeared')
 ######## End Spec
-
         worker = Worker(self.start_stream,)
         self.threadpool.start(worker)
 
@@ -224,197 +212,10 @@ class ApplicationWindow(QtWidgets.QWidget):
         except:
             pass
 
-
-    # def pb0(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Drumb/Octave/Bass-Drum-1.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Drumb/Solo/E-Mu-Proteus-FX-Wacky-Snare.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Drumb/Spare/Bamboo.wav')
-    #         pygame.mixer.music.play()
-    # def pb1(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex() == 0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/A.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/1.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/1.wav')
-    #         pygame.mixer.music.play()
-    # def pb2(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/e.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/2.wav')
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/2.wav')
-    #         pygame.mixer.music.play()
-    # def pb3(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/f#.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/3.wav')
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/3.wav')
-    #         pygame.mixer.music.play()
-    # def pb4(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/f.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/4.wav')
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/4.wav')
-    #         pygame.mixer.music.play()
-    # def pb5(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/g#.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/5.wav')
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/5.wav')
-    #         pygame.mixer.music.play()
-    # def pb6(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/g.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/6.wav')
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/6.wav')
-    #         pygame.mixer.music.play()
-    # def pb7(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/g1.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/7.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/7.wav')
-    #         pygame.mixer.music.play()
-    # def pb8(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/g2.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/8.wav')
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/8.wav')
-    #         pygame.mixer.music.play()
-    # def pb9(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/g3.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/9.wav') ##unknow wave data format
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/9.wav')
-    #         pygame.mixer.music.play()
-    # def pb10(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/g4.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/10.wav')
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/10.wav')
-    #         pygame.mixer.music.play()
-    # def pb11(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/g5.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex() == 1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/11.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/11.wav')
-    #         pygame.mixer.music.play()
-    # def pb12(self.ui):
-    #      if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Piano/Octave/a#.wav')
-    #         pygame.mixer.music.play()
-    #      elif self.ui.comboBox_for_tunes.currentIndex()==1:
-    #         pygame.mixer.music.load('instruments/Piano/Solo/12.wav')
-    #         pygame.mixer.music.play()
-    #      else:
-    #         pygame.mixer.music.load('instruments/Piano/Spare/12.wav')
-    #         pygame.mixer.music.play()
-
-    # def g1(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Guitar/Octave/GTR1.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex()==1:
-    #         pygame.mixer.music.load('instruments/Guitar/Solo/1.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Guitar/Spare/1.wav')
-    #         pygame.mixer.music.play()
-
-    # def g2(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Guitar/Octave/cleanchord-B-02.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex()==1:
-    #         pygame.mixer.music.load('instruments/Guitar/Solo/5.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Guitar/Spare/2.wav')
-    #         pygame.mixer.music.play()
-
-    # def g3(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Guitar/Octave/cleanmidbass-B02-slap.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex()==1:
-    #         pygame.mixer.music.load('instruments/Guitar/Solo/2.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Guitar/Spare/3.wav')
-    #         pygame.mixer.music.play()
-    # def g4(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Guitar/Octave/cleanmidbass-E01-slap.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex()==1:
-    #         pygame.mixer.music.load('instruments/Guitar/Solo/3.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Guitar/Spare/4.wav')
-    #         pygame.mixer.music.play()
-    # def g5(self.ui):
-    #     if self.ui.comboBox_for_tunes.currentIndex()==0:
-    #         pygame.mixer.music.load('instruments/Guitar/Octave/chord-G.wav')
-    #         pygame.mixer.music.play()
-    #     elif self.ui.comboBox_for_tunes.currentIndex()==1:
-    #         pygame.mixer.music.load('instruments/Guitar/Solo/4.wav')
-    #         pygame.mixer.music.play()
-    #     else:
-    #         pygame.mixer.music.load('instruments/Guitar/Spare/5.wav')
-    #         pygame.mixer.music.play()
-    
-
     def Volume_Control(self):
         value = int(self.ui.verticalSlider_For_volume.value())
         self.media.audio_set_volume(value*10)
         logging.info('volume changed')
-
 
     def stop_media(self):
         self.media.pause()
